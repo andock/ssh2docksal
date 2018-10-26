dd7ac01375df6f9e7e483d6322b21c9f545d8b22
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-10-26 13:51:41 +0200

    Fix travis test. Fix startup.sh

--- tests/startup.sh
+++ tests/startup.sh
@@ -3,6 +3,6 @@ docker run \
 -d \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /usr/bin/docker:/usr/bin/docker \
--v /home/cw/.ssh/authorized_keys:/root/.ssh/authorized_keys \
+-v ${HOME}/.ssh/authorized_keys:/root/.ssh/authorized_keys \
 -p 192.168.64.100:2222:2222 andockio/ssh2docksal
 
