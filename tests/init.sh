376d7380e1be0fca48714c50c1e2793db87085d0
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-10-26 13:32:15 +0200

    Fix travis tests.

--- tests/init.sh
+++ tests/init.sh
@@ -27,6 +27,7 @@ cp authorized_keys ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/authorized_keys
-
+ssh-add id_rsa
 # Start ssh2docksal docker image
 ./startup.sh
+
