89dcc6f17a30ee46ff8ce1d2656936e889541a9a
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-10-28 22:13:57 +0100

    Fix id_rsa key permission.

--- tests/init.sh
+++ tests/init.sh
@@ -11,7 +11,7 @@ cp id_rsa.pub ~/.ssh/id_rsa.pub
 cp id_rsa ~/.ssh/id_rsa
 cp authorized_keys ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/authorized_keys
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
