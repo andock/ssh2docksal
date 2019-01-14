70f19bb1e4dac66aa43ecd8eff902654413d300e
Christian Wiedemann <christian.wiedemann@key-tec.de>
2019-01-14 22:20:37 +0100

    Use --mount instead -v to bind authorized_key files

--- tests/startup.sh
+++ tests/startup.sh
@@ -8,7 +8,7 @@ docker run \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /usr/bin/docker:/usr/bin/docker \
 --name andock-ssh2docksal \
--v ${HOME}/.ssh/authorized_keys:/home/docker/.ssh/authorized_keys \
+--mount type=bind,src=${HOME}/.ssh/authorized_keys,dst=/home/docker/.ssh/authorized_keys \
 -p 192.168.64.100:2222:2222 andockio/ssh2docksal --verbose
 
 
